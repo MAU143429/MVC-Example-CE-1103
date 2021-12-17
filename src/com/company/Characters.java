c7dc7e93a514ace27b25cedcf1a9f8c18f8f45e7
MAU <mau141226@gmail.com>
2021-12-16 21:29:59 -0600

    [Update] Character class created

---
+++ src/com/company/Characters.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Characters {
+
+    private String pass;
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
